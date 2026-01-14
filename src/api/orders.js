2fa7066bd4d5ceb933cc3cfa821be9e3607a7cc2
aayush <ar0671362@gmail.com>
2026-01-14 22:15:33 +0530

    added order page

--- src/api/orders.js
+++ src/api/orders.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Base URL setup
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://sohwais-be.onrender.com/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
